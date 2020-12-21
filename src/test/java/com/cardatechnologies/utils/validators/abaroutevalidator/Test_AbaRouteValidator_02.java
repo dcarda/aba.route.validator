808572ec648eb29e3ce45f17b0f2650219b9a503
Daniel Carda <GoneBy2025@CardaTechnologies.com>
2020-12-20 22:47:42 -0700

    Improved license wording.

--- src/test/java/com/cardatechnologies/utils/validators/abaroutevalidator/Test_AbaRouteValidator_02.java
+++ src/test/java/com/cardatechnologies/utils/validators/abaroutevalidator/Test_AbaRouteValidator_02.java
@@ -3,8 +3,22 @@
  * Date:   2015/01/15
  * ---------------------------------------------------------------------------------------
  *
- * Copyright, (c) 2020 - Carda Technologies, LLC
- * All rights reserved.
+ *  License: Apache 2.0
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use
+ *  this file except in compliance with the License.
+ *
+ *  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software distributed under
+ *  the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS
+ *  OF ANY KIND, either express or implied including the implied warranties of
+ *  merchantability and fitness for a particular purpose..
+ *
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
  */
 
 package com.cardatechnologies.utils.validators.abaroutevalidator;
